a9f7901663171f3ef3bfafaeeab0af5f7cb6cd34
distolma <djmakaron2009@gmail.com>
2018-03-03 23:42:15 +0200

    fin name and description 🤯

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'unpapper-root',
+  selector: 'unpaper-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'unpapper';
+  title = 'unpaper';
 }
