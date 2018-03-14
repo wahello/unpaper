c55380174713a170e598691b89f6b1cf0c3f2884
distolma <djmakaron2009@gmail.com>
2018-03-14 07:58:40 +0200

    format html and sanitize url

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'unpaper-card',
@@ -9,6 +10,12 @@ import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from 
 export class CardComponent {
   @Input() public photo;
   @Output() public setImage = new EventEmitter();
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  public sanitizeURL(url: string) {
+    return this.sanitizer.bypassSecurityTrustStyle(`url(${url})`);
+  }
 
   public setImageHandler() {
     this.setImage.emit(this.photo);
