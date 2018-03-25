df8cab0cf78915c0053c8ceab5fd189328111a75
distolma <djmakaron2009@gmail.com>
2018-03-25 11:30:12 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [];
+import { SettingsComponent } from './settings/settings.component';
+import { HistoryComponent } from './history/history.component';
+import { PopularComponent } from './popular/popular.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'popular', component: PopularComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CardComponent } from './card/card.component';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AboutComponent } from './about/about.component';
+import { HistoryComponent } from './history/history.component';
+import { PopularComponent } from './popular/popular.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent],
+  declarations: [
+    AppComponent,
+    CardComponent,
+    SidenavComponent,
+    HomeComponent,
+    SettingsComponent,
+    AboutComponent,
+    HistoryComponent,
+    PopularComponent,
+  ],
   imports: [BrowserModule, SharedModule, AppRoutingModule, AppStoreModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
