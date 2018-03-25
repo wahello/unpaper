30c242a5c21812f9cbc86097cadc638dc8562792
distolma <djmakaron2009@gmail.com>
2018-03-25 11:28:14 +0300

    add popular component

---
+++ src/app/popular/popular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'unpaper-popular',
+  templateUrl: './popular.component.html',
+  styleUrls: ['./popular.component.scss']
+})
+export class PopularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
